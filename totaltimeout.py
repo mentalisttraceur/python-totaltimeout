d644762f7e33ca00c7345594477b749c708365fe
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2021-10-01 06:36:15 +0000

    SPDX identifier; module dunders before imports

--- totaltimeout.py
+++ totaltimeout.py
@@ -1,16 +1,5 @@
+# SPDX-License-Identifier: 0BSD
 # Copyright 2018 Alexander Kozhevnikov <mentalisttraceur@gmail.com>
-#
-# Permission to use, copy, modify, and distribute this software for any
-# purpose with or without fee is hereby granted.
-#
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-
 
 """Get timeouts right, without the hassle.
 
@@ -20,12 +9,11 @@ after some time has already passed. This helps correctly combine
 multiple operations which take timeout arguments.
 """
 
+__version__ = '3.0.1'
+__all__ = ('Timeout', 'TimeoutIterator')
+
 
 from time import time as _time
-
-
-__version__ = '3.0.0'
-__all__ = ('Timeout', 'TimeoutIterator')
 
 
 def _name(obj):
